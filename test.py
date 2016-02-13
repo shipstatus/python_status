35a9b8b4cd30e2ab57edd32b85bdb9cead527ff6
harsh157 <harsh12213@gmail.com>
2016-02-13 16:20:28 +0530

    add test for services

--- test.py
+++ test.py
@@ -1,13 +1,35 @@
 import unittest
-from app import App
+from couch import Couch
+from rabbit import Rabbit
+from postgres_service import Postgres
+from rethink import Rethink
 
 class TestSuite(unittest.TestCase):
 
-    def test(self):
-        app = App()
-        app.calculate()
-        self.failIf(app.retrieve() != 62)
+    def test_couch(self):
+        couch = Couch()
+        couch.populate()
+        things = couch.count()
+        self.failIf(things != 5)
 
+    def test_rabbit(self):
+        rabbit = Rabbit()
+        rabbit.sendMessage()
+        things = rabbit.relayMessage()
+        self.failIf(things != "Hello World!")
+
+    def test_postgres(self):
+        pg = Postgres()
+        pg.populate()
+        count = pg.read()
+        self.failIf(count != 5)
+        pg.disconnect()
+
+    def test_rethink(self):
+        rethink = Rethink()
+        rethink.populate()
+        things = rethink.count()
+        self.failIf(things != 5)
 
 def main():
     unittest.main()
